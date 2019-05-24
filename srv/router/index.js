e2eef9f76e5ba343768d146ef20042be2e02bcbe
gazinurka <ynicor@gmail.com>
2019-05-24 12:46:59 +0000

    9.12

--- srv/router/index.js
+++ srv/router/index.js
@@ -2,10 +2,13 @@
 /*eslint-env node, es6 */
 "use strict";
 
-module.exports = (app, server) => {
+module.exports = function (app, server) {
 	app.use("/node", require("./routes/myNode")());
 	app.use("/node/ex2", require("./routes/ex2")());
 	let express = require("express");
 	app.use("/node/os/web",express.static("os_web"));
 	app.use("/node/os", require("./routes/os")());
+	app.use("/node/excAsync", require("./routes/exerciseAsync")(server));
+	app.use("/node/JavaScriptBasics",require("./routes/JavaScriptBasics")());
+	app.use("/node/textBundle",require("./routes/textBundle")());
 };
